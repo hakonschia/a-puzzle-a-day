104dd49204da3a2001f3b5ea4eae4c03f6f82447
Håkon Schia <hakonschia5@gmail.com>
2026-01-18 14:30:18 +0100

    Generate all permutations of the different pieces. This is probably kind of weird since permutations of a piece is technically the same piece but it is just flipped in some way, and now every permutation is shown below the board. But this is probably going to make it way easier to create a solver later

--- app/src/main/java/com/example/apuzzleaday/Piece.kt
+++ app/src/main/java/com/example/apuzzleaday/Piece.kt
@@ -27,48 +27,158 @@ val pieces = listOf(
      * .
      * .
      */
-    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 1 to 0, 2 to 0), color = Color.Red.copy(alpha = 0.25f)),
+    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 1 to 0, 2 to 0), color = Color.Red.copy(alpha = 0.25f)).generateAllPermutationsForPiece(),
 
     /**
      * . . .
      * . . .
      */
-    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 1 to 0, 1 to 1, 1 to 2), color = Color.Green.copy(alpha = 0.5f)),
+    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 1 to 0, 1 to 1, 1 to 2), color = Color.Green.copy(alpha = 0.5f)).generateAllPermutationsForPiece(),
 
     /**
      * . . .
      *     . .
      */
-    Piece(listOf(0 to 0, 0 to 1, 0 to 1, 0 to 2, 1 to 2, 1 to 3), color = Color.Blue.copy(alpha = 0.5f)),
+    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 1 to 2, 1 to 3), color = Color.Blue.copy(alpha = 0.5f)).generateAllPermutationsForPiece(),
 
     /**
      * . . . .
      *   .
      */
-    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 0 to 3, 1 to 1), color = Color.Yellow.copy(alpha = 0.5f)),
+    Piece(listOf(0 to 0, 0 to 1, 0 to 2, 0 to 3, 1 to 1), color = Color.Yellow.copy(alpha = 0.5f)).generateAllPermutationsForPiece(),
 
     /**
      * .   .
      * . . .
      */
-    Piece(listOf(0 to 0, 0 to 2, 1 to 0, 1 to 1, 1 to 2), color = Color.Magenta.copy(alpha = 0.5f)),
+    Piece(listOf(0 to 0, 0 to 2, 1 to 0, 1 to 1, 1 to 2), color = Color.Magenta.copy(alpha = 0.5f)).generateAllPermutationsForPiece(),
 
     /**
      * .
      * . . .
      *     .
      */
-    Piece(listOf(0 to 0, 1 to 0, 1 to 1, 1 to 2, 2 to 2), color = Color.Cyan.copy(alpha = 0.5f)),
+    Piece(listOf(0 to 0, 1 to 0, 1 to 1, 1 to 2, 2 to 2), color = Color.Cyan.copy(alpha = 0.5f)).generateAllPermutationsForPiece(),
 
     /**
      * .
      * . . . .
      */
-    Piece(listOf(0 to 0, 1 to 0, 1 to 1, 1 to 2, 1 to 3), color = Color.Cyan.copy(alpha = 0.35f)),
+    Piece(listOf(0 to 0, 1 to 0, 1 to 1, 1 to 2, 1 to 3), color = Color.Cyan.copy(alpha = 0.35f)).generateAllPermutationsForPiece(),
 
     /**
      * . .
      * . . .
      */
-    Piece(listOf(0 to 0, 0 to 1, 1 to 0, 1 to 1, 1 to 2), Color.Cyan.copy(alpha = 0.2f))
-)
+    Piece(listOf(0 to 0, 0 to 1, 1 to 0, 1 to 1, 1 to 2), Color.Cyan.copy(alpha = 0.2f)).generateAllPermutationsForPiece()
+).flatten()
+
+private fun Piece.generateAllPermutationsForPiece(): Set<Piece> {
+    // A Boolean matrix where true means the piece is at the coordinate and false means it's not
+    val originalMatrix = List(height) { outerIndex ->
+        List(width) { innerIndex ->
+            coordinates.contains(innerIndex to outerIndex)
+        }
+    }
+
+    // Depending on the piece this will create duplicates, which is why we put them in a Set and not a List :)
+    return setOf(
+        originalMatrix.toPiece(color),
+        originalMatrix.flipVertically().toPiece(color),
+        originalMatrix.flipHorizontally().toPiece(color),
+        originalMatrix.flipVerticallyAndHorizontally().toPiece(color),
+        originalMatrix.transpose().toPiece(color),
+        originalMatrix.transpose().flipVertically().toPiece(color),
+        originalMatrix.transpose().flipHorizontally().toPiece(color),
+        originalMatrix.transpose().flipVerticallyAndHorizontally().toPiece(color)
+    )
+}
+
+private fun List<List<Boolean>>.toPiece(color: Color): Piece {
+    return Piece(
+        color = color,
+        coordinates = buildList {
+            this@toPiece.forEachIndexed { outerIndex, list ->
+                list.forEachIndexed { innerIndex, b ->
+                    if (b) {
+                        add(outerIndex to innerIndex)
+                    }
+                }
+            }
+        }
+    )
+}
+
+private fun List<List<Boolean>>.flipVertically(): List<List<Boolean>> {
+    /**
+     * .
+     * .
+     * . .
+     *   .
+     * ->
+     *   .
+     * . .
+     * .
+     * .
+     */
+    return reversed()
+}
+
+private fun List<List<Boolean>>.flipHorizontally(): List<List<Boolean>> {
+    /**
+     * .
+     * .
+     * . .
+     *   .
+     * ->
+     *   .
+     *   .
+     * . .
+     * .
+     */
+    return map { it.reversed() }
+}
+
+private fun List<List<Boolean>>.flipVerticallyAndHorizontally(): List<List<Boolean>> {
+    /**
+     * .
+     * .
+     * . .
+     *   .
+     * ->
+     * .
+     * . .
+     *   .
+     *   .
+     */
+    return flipVertically().flipHorizontally()
+}
+
+private fun List<List<Boolean>>.transpose(): List<List<Boolean>> {
+    /**
+     * .
+     * .
+     * . .
+     *   .
+     * ->
+     * . . .
+     *     . .
+     */
+
+    // If the matrix is n*m, n might != m, to simplify this we just create a n*n matrix (this code assumes the outer list is
+    // always larger, which we probably shouldn't do, but whatever)
+
+    val newMatrix = List(size) {
+        List(size) { false }.toMutableList()
+    }
+
+    // A transposition just flips the value of the indices, ie. new[innerIndex][outerIndex] = old[outerIndex][innerIndex]
+    indices.forEach { outerIndex ->
+        get(outerIndex).forEachIndexed { innerIndex, value ->
+            newMatrix[innerIndex][outerIndex] = value
+        }
+    }
+
+    // Remove redundant rows where everything is false
+    return newMatrix.filter { it.any { value -> value } }
+}
